04269640ba6c5b388a3c620b838bfbdc6bc4a865
Hunter Marshall <Beidah@gmail.com>
2025-02-22 21:14:56 -0600

    Add list of overworld regions basic data

---
+++ worlds/oot_soh/Regions.py
@@ -0,0 +1,14 @@
+import typing
+
+from BaseClasses import Region, MultiWorld
+
+
+class SoHRegion(Region):
+    game: str = "Ocarina of Time (SoH)"
+
+    def __init__(self, name: str, player: int, game: MultiWorld):
+        super(SoHRegion, self).__init__(name, player, game)
+        self.scene: str
+        self.time_passes: bool
+        self.locations: typing.List[str]
+        self.exits: typing.List[str]
